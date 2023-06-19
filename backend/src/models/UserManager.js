640150749abdd8ff02e62a0e58730ea8e0f28431
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-06-19 17:14:06 +0200

    add comment

--- backend/src/models/UserManager.js
+++ backend/src/models/UserManager.js
@@ -5,12 +5,16 @@ class UserManager extends AbstractManager {
     super({ table: "user" });
   }
 
-  // registerUser(name, email, password) {
-  //   return this.connection.query(
-  //     `insert into ${this.table} (name, email, password) VALUES (?, ?, ?)`,
-  //     [name, email, password]
-  //   );
-  // }
+  /**
+   *
+   * Nous pouvons créer n'importe quelle méthode, n'importe quand.
+   * Donc moi, maintenant, je souhaite créer une méthode pour enregistrer un user
+   * J'appel donc `registerUser`, celui prend 1 paramètre, c'est un obj `user`
+   * qui contiendra les informations suivantes :
+   * - name
+   * - email
+   * - password -> celui ci, sera le password hashé bien entendu
+   */
 
   registerUser(user) {
     return this.connection.query(
@@ -19,6 +23,21 @@ class UserManager extends AbstractManager {
     );
   }
 
+  /**
+   * Une autre façon de faire
+   */
+  // registerUser(name, email, password) {
+  //   return this.connection.query(
+  //     `insert into ${this.table} (name, email, password) VALUES (?, ?, ?)`,
+  //     [name, email, password]
+  //   );
+  // }
+
+  /**
+   * Pour le bien de mon projet, j'ai du créer une méthode findByMail
+   * Pourquoi ?
+   * Pour retrouver un user via son email, si j'avais besoin d'autre chose, alors je peux créer autant de fonction que necessaire
+   */
   findByMail(email) {
     return this.connection.query(
       `SELECT * FROM ${this.table} WHERE email = ?`,
